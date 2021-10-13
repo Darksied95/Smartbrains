0de4a97df572e165df0034e854fc66b701a12702
Darksied95 <aliyuefe9501@gmail.com>
2021-10-13 20:04:40 +0100

    Added Rank,Logo,ImageLinkForms

--- src/App.js
+++ src/App.js
@@ -1,15 +1,41 @@
 import React, {Component}from 'react';
+import Particles from 'react-particles-js';
 import './App.css';
 import Navigation from './Components/Navigation/Navigation';
 import Logo from './Components/Logo/Logo';
+import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm'
+import Rank from './Components/Rank/Rank';
+
+
+const particlesOptions={
+  particles: {
+    number:{
+      value:30,
+      density:{
+        enable:true,
+        value_are:800
+      }
+    },
+    line_linked: {
+      shadow: {
+        enable: true,
+        color: "#3CA9D1",
+        blur: 5
+      }
+    }
+  }};
+
 class App extends Component {
   render(){
   return (
     <div className="App">
+      <Particles className='particles'
+            params={particlesOptions}   />
       <Navigation />
       <Logo />
-      {/*<ImageLinkForm />
-      <FaceRecognition />*/}
+      <Rank />
+      <ImageLinkForm />
+      {/*<FaceRecognition />*/}
     </div>
   );
 
--- src/Components/Logo/Logo.js
+++ src/Components/Logo/Logo.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Tilt from 'react-tilt';
 import './Logo.css';
+import brain from './Brain.png'
 
 const Logo =()=>{
     return(
         <div className="ma4 mt0">
-            <Tilt className="Tilt Tiltcss br2 shadow-2" options={{ max : 25 }} style={{ height: 150, width: 150 }} >
-            <div className="Tilt-inner"> 👽 </div>
+            <Tilt className="Tilt Tiltcss br2 shadow-2" options={{ max : 55 }} style={{ height: 150, width: 150 }} >
+            <div className="Tilt-inner pa3"> <img style={{paddingTop:'5px'}} alt="logo" src={brain} /> </div>
             </Tilt>
         </div>
     );
