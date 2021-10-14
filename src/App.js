6383fe2f90b1d70cfc03b1eb6b7177853771c6e8
Darksied95 <aliyuefe9501@gmail.com>
2021-10-14 08:46:41 +0100

    New folder added

--- src/App.js
+++ src/App.js
@@ -26,6 +26,17 @@ const particlesOptions={
   }};
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state={
+      input:'',
+    }
+  }
+  
+  onInputChange = (event) => {
+    console.log(event);
+  };
+
   render(){
   return (
     <div className="App">
@@ -34,7 +45,7 @@ class App extends Component {
       <Navigation />
       <Logo />
       <Rank />
-      <ImageLinkForm />
+      <ImageLinkForm onInputChange={this.onInputChange}/>
       {/*<FaceRecognition />*/}
     </div>
   );
