c8a3800feb3b4ea12f6ca05925ebcae2eb95a071
Darksied95 <aliyuefe9501@gmail.com>
2021-12-27 18:29:12 +0100

    Npm serve added

--- src/App.js
+++ src/App.js
@@ -92,7 +92,7 @@ class App extends Component {
     .then(response => response.json())
     .then(response =>{
       if(response){
-      fetch('https://enigmatic-atoll-64959.herokuapp.com:3000/image', {
+      fetch('https://enigmatic-atoll-64959.herokuapp.com/image', {
         method: 'put',
         headers: {'Content-Type': ' application/json'},
         body : JSON.stringify({
